d561edf3c3c19179b4d95b5e9764967aee161559
xu.wang <867103701@qq.com>
2025-04-25 23:18:29 +0800

    更换tap.

--- app/src/main/java/com/chocolate/nigerialoanapp/global/App.kt
+++ app/src/main/java/com/chocolate/nigerialoanapp/global/App.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.text.TextUtils
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.DeviceUtils
@@ -13,6 +14,7 @@ import com.blankj.utilcode.util.SPUtils
 import com.blankj.utilcode.util.Utils.OnAppStatusChangedListener
 import com.chocolate.nigerialoanapp.BuildConfig
 import com.chocolate.nigerialoanapp.collect.LocationMgr
+import com.chocolate.nigerialoanapp.collect.utils.AESUtil
 import com.chocolate.nigerialoanapp.log.LogSaver
 import com.chocolate.nigerialoanapp.utils.AppsflyerUtils
 import com.chocolate.nigerialoanapp.utils.FirebaseUtils
@@ -86,8 +88,43 @@ class App : Application() {
         SPUtils.getInstance().put(LocalConfig.LC_APPSFLYER_ID, AppsflyerUtils.AF_DEV_KEY)
 
         //https://docs.easeid.ai/#/zh-cn/liveness-detection/liveness-detection
-        val appId = "K6913783808"
-        val EASE_CONFIG_PRIVATE_KEY = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDCMmS1J6Qfu3CGzS5k0IeildTG8ZtIw1pvgQEdqoNOhCHsGT/nastXL4skM/IOmepETdWdoHqaD1qIPuTz9RK4wOpru//GgdXXyl9DN5rPuzZeyKgIBKTCH6cbS71wCb/QHGr6yrVmmu55e/fVDgtj2/KwvvdqD+u5owusmOVaS5+cfw7vxkjnxKRdZHzWpRiPJLYj7WRQH4ygy5OeY4AHOcxSiDFwK3A2Vat2dbcYLGjeoNu0GrnOUQsZ/osXz4koy8TZzvDBHWw8sD6TFG5CMATeNP+11UDy+tBXieIgXJXpLDgAbYVOVmDdDvevBiqDXULzvJ4xD+k7ct7URM9RAgMBAAECggEANCzl8eYcqonpyc4G0P9V8dDfwjPXzMzv11WJvza44LePhaejC8Idr+cOM0PhnRqtXnyrEBa12f6WDFUZnpIR3aG3WgrAxczMb5Xn0l92MKnUCdNPkhftTFrEwwiudROpfXilJYyhAFhQCkPMamnDlajao7IrN0vfwZiU4mKR/JgtBlmy77VZwDL982W2C/mlwcAbZbDoTY827EOE1Xt8dpGBmKlN1cDuOlhC1M7gnW1qO6YmZJ8eSQmtCFrto1b6zcik5aFA5bfTdbN5ctCpeJ0pbBHYyDmsHqkvpZar6v5n1YyQzCC7zqgoCfXezPQReg+reg8xxLanL8E+G0lQAQKBgQDlPY1uFjp+E5AcUI93pGWtIIS+FH/oN7WrxeVyjY/FwVKmk6Pw86jz3wwYua/2PLEskOfSx5Ps8DrrQQTmvNSvliAkagRN8mivIXjD17AYccx89rm1Zq0zZ7xsFb6eOXbdqciXe9W3YjjM9YXRyFZS+RHKOLoSro4JBP/ZhNm08QKBgQDY3Z/1nrBK7DZrWEO6JrzHCR3b1Cm0aX08sKSDkP8zFw8n7I9AJrC4MYIkKyBlm2w8hVBTUpE52TOyBUpGSQA6KG213Or95Mtm9Lu8tH+45GoDwd2eXyvxvaU8Jcmdb+CtX4sUlgRwiJy5cSlpyRosPdypydvfxz2pOIliLTtAYQKBgGjWoZu7Jr0KDVolrbc1xBpNSOf1UBnPgJIySyD8hMgYBciQtOU5Sgdd9pZINaUTVKPhEWRgSdKOeyOj9K3hphM3QTPmGFndhuqxEB3gwnQrdy2fWIcRkhx0UMPNngOWnDn7r/U3eSIJFZkGoFC0omYH6o7lKpPoqKSPWMMN8tLBAoGAI8Vary7/sTwJNeeXNEtTdBki3bG/N0z3JV2VpUXzcj/pdhJlBpXbLGauKr0aWOgsEEeFGllSLxKOnlyVBcbgzLcCca8A6+QjUNYpaVT0hFDY08nUfl0AaJ15ddRdzyQ3ESHjVP/xt4YxI71wsUxEjuudTDxJkj3RA+qZbqI7VcECgYAwvDXRK8IjxNS+GwZ/SmXD+EUgetKffGJuxSnxYoOTrHxW0PO7i74c8220fqJWvA9Llo7LQXDVt7BRNSvPAzkwc2byAnL/uW9d7o2k03yI1qZxr9k42Z3ULd9zNZ9HM0JmRcIotK3NYcEmaphTxgwFkM+yJ2ZZUaRI4pRx7PzbzQ=="
+        val appDecId = "LcXoMKIG9qXKgvwL02b8bw=="
+        val appId = AESUtil.decrypt(appDecId)
+        val EASE_CONFIG_PRIVATE_KEY_dec = "E97e74RS68lXybSkt0DKZsvKw8fJZV6S7k7hjJ1HkSrUHXILfhD/iMkMTUK+9aFE0HWWU7MHiIhY\n" +
+                "MKyNjqj5P172O+UtNUBmcPLpQrh/YFscQsdSbiuCbWGVfT0mqQ9efVAhKCZZCWFhhKWxkdAFXbDN\n" +
+                "O0MaCpMds17XYH29n9ekdZ1tm+j2R8qFtY7/oca7JtvGZJjN5wIxq+xCnli6S58suAfH2Zz39uCw\n" +
+                "IN21BQHa4iQUa0bCWgf/DxQk1Cvb1gzXGK35RFK7w529pOJ2wNHZZSs+d5P9lZhU+yrmAqSOZSAN\n" +
+                "jcn89lnl4yzB1mDzaD6Q6L+NdejSr+nk6VwDCZHmmv2myTSacaXp8E5STK+ZJ+IQJzD0U2Gqrh6O\n" +
+                "+5RpaEtKQ6w0wAXqqcfEV4OKg7vc2m+lONhrF/YJ4rcKXBgRmcs6L9Kpr34/tQdhi+yZ1iC+PWGq\n" +
+                "rdFcf+ZQFYddyVxh1mtRUwYfOnLV0ROkRgMcBJDT6lKDkxMVCrGHaYRrmyEvgQy80hXaBFEDOTXY\n" +
+                "Z1uUSgbC5KhNcY+EHSHvAYwG4eJId/ucocFpdgyoRbWpjHrk9Ge6NmqOZ21TpcQCMt+vN4GaXK8q\n" +
+                "q33gH2wVX2y4cpiH+mqeLyoKENch4XFpYGl/LBlqpg/eIERJ5cnMw1pGpcpcIndNBlBl0rhKlKIy\n" +
+                "ez9pMQgLxNB712XagM3XETsDhOP2aHYE04KBYl3LkkdJph4jG5Bi7Bh9UZQhXMDjORDW9KgeTcmF\n" +
+                "juOwsn8SsTtmgkxBeb7nlNLlNulsM9i2nLd4hTOyg3k7N125mBld6EGD1Gojm6ZwndixHzwSjlmh\n" +
+                "Txeu3HSoV/7wbdnK/VteYQjolbOCPZQCBIiEzk1GwgMumxdHsxOlYwI3WhNk1iPnNydyTu0N2StO\n" +
+                "iDP9p7gaUgUNLE/BFGJvi0WObnvSjzXUA5STWKRf6v93TsO/mFrd0kE+33/VnfhLOTp8/M59Bub3\n" +
+                "cjFhGi/Ra31oI8zUDWriD7xXcnOEf3kJmvJZTrwTtkPidW1dnhbMZ+2zVHjKHmsc+MWIPCFo4vu4\n" +
+                "yrQEksW2PX8WSO0F6kcxfpOfs0Kn22AFG4WUQDJ65m+49tzVrt3C7D7GHam2jFUsW5LGcGnN/sfi\n" +
+                "Bjo2TrhGgugeES94rjNbObe1byMG18vWzwKuN6lV1POXPLdgY9c/srHhc08wejk3pLRTCNkxmYWq\n" +
+                "37Z0tYx48L3ejcSlNOOXZnjb77Ysvd1OoBsFJ4WjlBQYgnBxl6Tx+j0+6U4iamiurr7ZzAht65fA\n" +
+                "99+yyU7AQsz3UnaoaH0Z2kA01WIbMsESqDVWdOJ3WFefyNpjZf5+VRAZx9zDdbeVHmbfgP+05bx5\n" +
+                "bsDtiL058FNE1VBzw+seCyeEqPIQbpwBc0YMcFDKgiiZdo9CoMzK7yBBo95q/QP4vyfdZoqyW9uh\n" +
+                "+GzgVb0dbrJygnT6zYeQDRQ64Yas7kWF9DoNXrWK/jBbbkCsFaEhHaBch7BkR3ac9ZIguvypF1SM\n" +
+                "LfdRkcl9+BvUjURX55C8CcPP3YpCbIAE9RLHuPdC/BNPz53pHZpLkRSLhQK4otzz+ArZ3p0J/rVD\n" +
+                "dP0C0ZNvQCoO/1m3OAF8Jv9VEe88hbyRoTdI6yOLj+KzkAFtlUy6r38HXS19LuV+hLUHfWgjztUY\n" +
+                "yxRUvozhPgTSDGFkw+Bk/kggRY5m5vcvmfHzG9QPG1pppMPeigoiqiXwRKuYld1FTfs43SE8ns3/\n" +
+                "BwTqyNEMgT9j9ywyLVrIyItoDcWlLTQt3GBNv5QbtmIxSeKbobLfwWmset1K5v0PHOsy3roFc37U\n" +
+                "ecXbLV7xI/qbkDP0I+FuhJx6oAYIBEXtKa1ZcY2QXwBkDjKTZcvvbXCyHrwzPHVdfBAnOjsFD5QQ\n" +
+                "SjAo3yXuOX2KyAtvQGJt+CF33CMbtWjcIBouaLFfQQVAS75PtHXjDAS+LTs1XdGW5q4GQ/wZm1KD\n" +
+                "QiHKHhn1V9zLcKxwp0lwfRVnU2AgmZ2Xvc4lXmrSF1aWSwEzGFn0HlCdHXdUVu01cs0Kzgbs8WIq\n" +
+                "GMauwZUXxsj+JIRQwg+ywaIRRS6tgXwg2QoYcMTeJa6hTkNuM5ETnGjG1t8hQvweqFzyvWVN9yd+\n" +
+                "j0E6Br7fVQRJ+dCNL8pGo3Yydyw2AhYZKB/xcDI2AabreufO"
+        val EASE_CONFIG_PRIVATE_KEY = AESUtil.decrypt(EASE_CONFIG_PRIVATE_KEY_dec)
+        if (BuildConfig.DEBUG) {
+            Log.e("Test", " -----------------------------------------------------    ")
+            Log.e("Test", "" + appId)
+            Log.e("Test", "" + EASE_CONFIG_PRIVATE_KEY)
+        }
         val config = EaseConfig(appId, EASE_CONFIG_PRIVATE_KEY,
             DeviceUtils.getUniqueDeviceId(), BuildConfig.DEBUG)
         EaseID.initialize(this, config)
